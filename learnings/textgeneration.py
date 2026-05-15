3697decf4f805e70fcc09757b4c2f9303a8c83b3
vasavi@1421 <vasavimg242@gmail.com>
2026-05-15 11:26:57 +0530

    learnings structured

--- learnings/textgeneration.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-gen=pipeline("text-generation", model="gpt2")
-result=gen("The future of AI in india is",max_new_token=50,num_return_sequences=1)
-print(result[0]['generated_text'])
-creative=gen("Once upon a time in mysuru",max_new_token=60,temperature=1.2,do_sample=True)
-focused=gen("Once upon a time in mysuru",max_new_token=60,temperature=0.3,do_sample=True)
-
-print("Creative:",creative[0]['generated_text'])
-print("Focused:",focused[0]['generated_text'])
